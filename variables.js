666cd4e2407e3c8f685fe62cb3907fa08b63133d
MarinaMSK <marina-moskaleva-1997@mail.ru>
2021-06-14 16:56:36 +0300

    Added changes

---
+++ variables.js
@@ -0,0 +1,23 @@
+//The task 5. The work with variables.
+let a = 17,
+    b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+//The task 7. The work with strings.
+//Create two variables with strings and display them in the console.
+let str1 = 'Hello,';
+let str2 = 'Word!';
+console.log(str1 + str2);
+
+/* The task 9. Create the variable "age" and assign to it your age.
+Display string in the console. */
+let age = 23;
+console.log('I am' + ' ' + age + ' ' + 'years old!');
+
+/* The task 10. Function 'prompt'.
+Ask for a user name and display a message on the browser screen. */
+
+alert('Your name:' + prompt('What is your name?'));
